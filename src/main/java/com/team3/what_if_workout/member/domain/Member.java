af23be823838b73e02cb826ca98db822ef1e775a
Gyuwon-Jung <licmm0520@gmail.com>
2025-11-25 20:03:41 +0900

    [feat] #1 User Entity 구현 및 서버 시작시 로그인창 끔

---
+++ src/main/java/com/team3/what_if_workout/member/domain/Member.java
@@ -0,0 +1,41 @@
+package com.team3.what_if_workout.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Integer goalsCalories;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Builder
+    public Member(String id, String password, Integer goalsCalories, String role) {
+        this.id = id;
+        this.password = password;
+        this.goalsCalories = goalsCalories;
+        this.role = role;
+    }
+}
