0f5ff3d473a0c793e88c5ca6c8fe0347cd06d6ed
Gyuwon-Jung <licmm0520@gmail.com>
2025-11-25 18:58:06 +0900

    initialize project

---
+++ src/main/java/com/team3/what_if_workout/WhatIfWorkoutApplication.java
@@ -0,0 +1,13 @@
+package com.team3.what_if_workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatIfWorkoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatIfWorkoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team3/what_if_workout/WhatIfWorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.what_if_workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIfWorkoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
