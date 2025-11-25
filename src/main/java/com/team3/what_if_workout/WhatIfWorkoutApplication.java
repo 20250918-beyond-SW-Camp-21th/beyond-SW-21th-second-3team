b97d8e1c3241d4695faa7fedaba6e8e4d79aae19
Gyuwon-Jung <licmm0520@gmail.com>
2025-11-25 19:37:21 +0900

    #1 BaseEntity 구성

--- src/main/java/com/team3/what_if_workout/WhatIfWorkoutApplication.java
+++ src/main/java/com/team3/what_if_workout/WhatIfWorkoutApplication.java
@@ -2,8 +2,10 @@ package com.team3.what_if_workout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WhatIfWorkoutApplication {
 
     public static void main(String[] args) {
