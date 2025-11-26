e6c55a66df6f064343d17ae3b8e7be06082ebd4d
Gyuwon-Jung <licmm0520@gmail.com>
2025-11-26 09:03:26 +0900

    [fix] #1 Member가 BaseEntity 상속하게끔 설정

--- src/main/java/com/team3/what_if_workout/member/domain/Member.java
+++ src/main/java/com/team3/what_if_workout/member/domain/Member.java
@@ -1,5 +1,6 @@
 package com.team3.what_if_workout.member.domain;
 
+import com.team3.what_if_workout.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
