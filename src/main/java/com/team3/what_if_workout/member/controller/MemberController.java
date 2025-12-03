75eb9c6c279e48e06b076cbd37f2845d7acfa47f
Gyuwon-Jung <licmm0520@gmail.com>
2025-12-03 18:43:52 +0900

    [fix] createAt 부분 업데이트 안되게 설정

--- src/main/java/com/team3/what_if_workout/member/controller/MemberController.java
+++ src/main/java/com/team3/what_if_workout/member/controller/MemberController.java
@@ -3,11 +3,16 @@ package com.team3.what_if_workout.member.controller;
 import com.team3.what_if_workout.member.domain.Member;
 import com.team3.what_if_workout.member.dto.reqeust.SignupDTO;
 import com.team3.what_if_workout.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +22,16 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입 API입니다.")
+    @ApiResponse(responseCode = "201", description = "스케줄 조회 성공",
+            content = @Content(mediaType = "application/json",
+                    examples = @ExampleObject(
+                            name = "회원가입 성공",
+                            value = "회원가입 되었습니다."
+                    )))
     @PostMapping("/signup")
-    public ResponseEntity registerUser(SignupDTO dto){
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity registerUser(SignupDTO dto) {
         Member member = memberService.signup(dto);
         return new ResponseEntity(HttpStatus.CREATED);
     }
