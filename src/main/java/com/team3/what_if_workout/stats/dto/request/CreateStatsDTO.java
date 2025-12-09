c40e41dd210c0490a117fe8fb744e645c91e4ea0
rlatjddms <kseo_o@naver.com>
2025-12-09 12:00:40 +0900

    #9 Report CRUD ãê  RUD 구현

--- src/main/java/com/team3/what_if_workout/stats/dto/request/CreateStatsDTO.java
+++
@@ -1,8 +0,0 @@
-package com.team3.what_if_workout.stats.dto.request;
-
-public record CreateRankingDTO(
-
-        Long userId,
-        Integer totalDuration,
-        Integer totalCalories
-) {}
--- src/main/java/com/team3/what_if_workout/stats/dto/respose/TimeRankingDto.java
+++
@@ -1,4 +0,0 @@
-package com.team3.what_if_workout.stats.dto.respose;
-
-public class TimeRankingDto {
-}
